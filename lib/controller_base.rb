4e2cf8b7bf9a57b770e84c62571cf05feaf1194c
Reilly Villanueva <reillyvillanueva@gmail.com>
2016-08-09 18:05:49 -0700

    started implementing router

--- lib/controller_base.rb
+++ lib/controller_base.rb
@@ -58,5 +58,9 @@ class ControllerBase
 
   # use this with the router to call action_name (:index, :show, :create...)
   def invoke_action(name)
+    Router.send(:name)
+    if res.status == 404
+      render name
+    end
   end
 end
